b7c5cfc53dc0fb1a0425cc0ef25ffe88f914afea
andyjianzhou <andyzhou727@gmail.com>
2025-10-31 16:45:30 -0600

    Adding delete city test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -41,4 +41,39 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
         assertTrue(list.hasCity(toronto));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCityNotInList() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        list.deleteCity(edmonton);
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCityMultipleCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        City toronto = new City("Toronto", "ON");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+        list.addCity(toronto);
+        list.deleteCity(edmonton);
+        assertTrue(list.hasCity(calgary));
+        assertFalse(list.hasCity(edmonton));
+        assertTrue(list.hasCity(toronto));
+    }
 }
