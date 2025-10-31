4b3be44a5aa87023e212e9db7ce6508fe45381c3
andyjianzhou <andyzhou727@gmail.com>
2025-10-31 16:40:36 -0600

    Adding hasCityTest

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,41 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testHasCityNotFound() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        assertFalse(list.hasCity(edmonton));
+    }
+
+    @Test
+    public void testHasCityEmptyList() {
+        CustomList list = new CustomList();
+        City toronto = new City("Toronto", "ON");
+        assertFalse(list.hasCity(toronto));
+    }
+
+    @Test
+    public void testHasCityMultipleCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        City toronto = new City("Toronto", "ON");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+        list.addCity(toronto);
+        assertTrue(list.hasCity(edmonton));
+        assertTrue(list.hasCity(calgary));
+        assertTrue(list.hasCity(toronto));
+    }
 }
